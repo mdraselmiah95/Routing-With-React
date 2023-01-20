513680299ae605903fc30f76bdbc79c9ec97819c
mdraselmiah95 <russelroy15@gmail.com>
2023-01-20 23:28:38 +0600

    new routing system added

--- version-6.7/src/layout/main/Main.js
+++ version-6.7/src/layout/main/Main.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Main = () => {
+  const { pathname } = useLocation();
   return (
     <div>
       <Navbar />
-      <div>
+      <div
+        className={` ${
+          pathname === "/" ? "max-w-[1600px] px-5" : "max-w-7xl"
+        } max-w-[1600px] mx-auto`}
+      >
         <Outlet />
       </div>
       <Footer />
